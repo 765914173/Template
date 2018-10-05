5d4916415cc26e95021ef0fef30d08f55125e5e7
charlie <765914173@qq.com>
2018-10-05 15:42:54 +0800

    update 5 page

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { IndexComponent } from './index/index.component';
+import { ProjectComponent } from './project/project.component';
+import { CartComponent } from './cart/cart.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'index', component: IndexComponent },
+  { path: 'project', component: ProjectComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'contactUS', component: ContactUsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -4,9 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from './home/home.component';
+import { IndexComponent } from './index/index.component';
+import { ProjectComponent } from './project/project.component';
+import { CartComponent } from './cart/cart.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    IndexComponent,
+    ProjectComponent,
+    CartComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
